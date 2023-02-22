edca4d03427ee3b3fac0896e5dfbaf36c09e164b
Ellen Houghton <ellen.a.ash@gmail.com>
2023-02-22 13:34:52 +0000

    add list comprehension to resolve upper_section function

--- yahtzee/yahtzee_core-game.py
+++ yahtzee/yahtzee_core-game.py
@@ -1,3 +1,12 @@
 import random
 
 dice_roll = [random.randrange(1, 7) for i in range(0, 5)]
+
+def upper_section(dice_roll):
+    list_to_sum = [dice_roll.count(i) *i for i in range(1,7)] # could this be a lambda function?
+    return sum(list_to_sum)
+    
+print(upper_section(dice_roll))
+
+def lower_section(dice_roll):
+    pass
