b47a93f771ec0edb7d52902f4331c4a626c2eba1
Ellen Houghton <ellen.a.ash@gmail.com>
2023-02-24 17:28:38 +0000

    bug still to fix in while loop, but scores updating to file

--- guess_game.py
+++ guess_game.py
@@ -3,25 +3,44 @@ from datetime import datetime
 from games.games import Games, Player
 
 ingame_message_dict = {
-    "get_ready": [" Ready    ", "Set   ", "Go\n"], 
     'welcome': ["Welcome to...\n", "Guess\t", "the\t", "Number\t\n\n"],
     'title': "Guess the Number",
     "sorry": "Sorry, I did not understand your choice. \n",
     'end_game_message': "Goodbye"
 }
 
-def play_game():
-    number = random.randint(0,11)
-    guess = input("I am thinking of a number between 0 and 10\nYou have 5 trys to guess.\nWhat do you think it is?")
-    count = 1
-    
-    while count < 6:
-        if guess != number:
-            play_game()
-        else:    
-            print("Congratulations, you guessed it in {count}.\nMy number was {number}.")
-    
+number = random.randint(0,11)
+print(number)
 
+def play_game():
+    guess = input("I am thinking of a number between 0 and 10\nYou have 5 trys to guess.\nWhat do you think it is?")
+    count = 0
+    while count != 4:
+        count += 1
+        if guess == str(number):    
+            print(f"Player wins\nCongratulations, you guessed it in {count}.\nMy number was {number}.")
+            score_tracker("player")
+            break
+        elif guess != str(number):
+            guess = input("Have another try...")
+    save_game()
+
+def score_tracker(winner):
+    if winner == "player":
+        guess_player.increase_score(1)
+    else:
+        guess_computer.increase_score(1)
+
+def save_game():
+    save = input("Do you wish to save the scores? Y or N")
+    if save in ["n", "N", 'q', "Q"]:
+        Games.end_game(ingame_message_dict["end_game_message"])
+    else:
+        now = datetime.now()
+        date_time_str = now.strftime("%Y-%m-%d %H:%M")
+        with open("saved_score.txt", 'a') as save_score:
+            save_score.write(f"{guess_game.get_title():20}  {guess_computer.get_score()}:{guess_player.get_score()}  {date_time_str}\n")
+        Games.end_game(ingame_message_dict["end_game_message"])
 
 guess_game = Games('guess')
 guess_game.set_title(ingame_message_dict["title"])
