e187624b5d074dc3ac41cb3881b013c5234ef91b
Ellen Houghton <ellen.a.ash@gmail.com>
2023-02-26 17:17:13 +0000

    merged

--- rps_core_game.py
+++ rps_core_game.py
@@ -1,10 +1,5 @@
 import random, time, sys
 from datetime import datetime
-<<<<<<< HEAD:rps_core_game.py
-import rps_score
-from games.games import Games, Player
-=======
->>>>>>> featuredev:core_game_rps.py
 
 ingame_message_dict = {
     "winner_c": "\tComputer is the WINNER\t\n", 
@@ -12,21 +7,11 @@ ingame_message_dict = {
     "paper_win": ">>>>> Paper wraps Rock<<<<<\n", 
     "scissors_win": ">>>>> Scissors cut Paper<<<<<\n", 
     "winner_u": "Congratulations you are the winner!", 
-<<<<<<< HEAD:rps_core_game.py
-    "draw": "\tIt was a draw\t\n", 
-    "get_ready": [" Ready    ", "Set   ", "Go\n"], 
-    'welcome': ["Welcome to...\n", "Rock\t", "Paper\t", "Scissors\t\n\n"],
-    'title': "Rock, Paper, Scissors",
-    "sorry": "Sorry, I did not understand your choice. \n",
-    'end_game_message': "Goodbye"
-    }
-=======
     "draw": "\t<<<<< DRAW >>>>>\t\n", 
     "get_ready": [" Ready    ", "Set   ", "Go\n"], 
     'welcome': ["Welcome to...\n", "Rock\t", "Paper\t", "Scissors\t\n\n"],
     "sorry": "Sorry, I did not understand your choice. \n",
                        }
->>>>>>> featuredev:core_game_rps.py
 
 # ABSTRACTION - the object here is the score and match count that I need to track through the game for both player and computer and not the game title etc
 # ENCAPSULATION - the score tracker is encapsulated here so it is not dragged through functions and over complicating the game code
@@ -84,27 +69,10 @@ class Game_rps:
         print("*" * 6 + "*" * len(winner_message) + "*" * 6)
         print()
 
-<<<<<<< HEAD:rps_core_game.py
-def play_game():
-    rps_score.thinking()
-    computer = make_choice(selector = "Computer")
-    human_input = input("\n\tR = Rock\n\tP = Paper\n\tS = Scissors\n\tQ = Quit\nPlease enter R, P, S or Q:\n")
-    user = make_choice(selector = "Player", data = human_input)
-    print(f"\tYou chose {user}\n\tand\t\n\tComputer chose {computer}\n")
-    winner = ""
-    if user == computer:
-        print(ingame_message_dict["draw"])
-        winner = "None"
-    elif user == "Rock":
-        if computer == "Paper":
-            print(ingame_message_dict["paper_win"], ingame_message_dict["winner_c"])
-            winner = "computer"
-=======
     def save_game(self):
         save = input("Do you wish to save the scores? Y or N ")
         if save in ["n", "N", 'q', "Q"]:
             self.end_game()
->>>>>>> featuredev:core_game_rps.py
         else:
             now = datetime.now()
             date_time_str = now.strftime("%Y-%m-%d %H:%M")
@@ -169,76 +137,7 @@ def play_game():
         if decision in ["Q", "q"]:
             self.save_game()
         else:
-<<<<<<< HEAD:rps_core_game.py
-            print(ingame_message_dict["paper_win"])
-            congratulations()
-            winner = "player"
-    else:  # if the human_player chooses Scissors
-        if computer == "Rock":
-            print(ingame_message_dict["rock_win"], ingame_message_dict["winner_c"])
-            winner = "computer"
-        else:
-            print(ingame_message_dict["scissors_win"])
-            congratulations()
-            winner = "player"
-    score_tracker(winner)
-    # new_player_score, new_computer_score = score_tuple #unpacking a tuple
-    play_again()
-
-def congratulations():
-    winner_message = ingame_message_dict["winner_u"]
-    print("*" * 6 + "*" * len(winner_message) + "*" * 6)
-    print("*" * 6 + winner_message + "*" * 6)
-    print("*" * 6 + "*" * len(winner_message) + "*" * 6)
-    print()
-
-# def end_game():
-#     print("Goodbye\n", "="*50)
-#     sys.exit()
-
-def play_again():
-    decision = input("Press enter to play again or Q to quit\n")  # Q is hardcoded so this text is not available to modify in the game dictionary
-    if decision in ["Q", "q"]:
-    #if decision == "Q" or decision == "q":
-        save_game()
-    else:
-        play_game()
-
-# def open_game():
-#     for item in ingame_message_dict['welcome']:
-#         time.sleep(0.85)
-#         print(item, end = '', flush = True)
-#     play_game(player_score = 0, computer_score = 0)
-
-def score_tracker(winner):
-    if winner == "computer":
-        rps_computer.increase_score(1)
-    else:
-        rps_player.increase_score(1)
-    print(f"Your score is {rps_player.get_score()} and the computer has {rps_computer.get_score()}.")
-
-def save_game():
-    save = input("Do you wish to save the scores? Y or N")
-    if save in ["n", "N", 'q', "Q"]:
-        Games.end_game(ingame_message_dict["end_game_message"])
-    else:
-        now = datetime.now()
-        date_time_str = now.strftime("%Y-%m-%d %H:%M")
-        with open("saved_score.txt", 'a') as save_score:
-            save_score.write(f"{rps_game.get_title():20}  {rps_computer.get_score()}:{rps_player.get_score()}  {date_time_str}\n")
-        Games.end_game(ingame_message_dict["end_game_message"])
-
-rps_game = Games('rps')
-rps_game.set_title(ingame_message_dict["title"])
-
-rps_game.open_game(ingame_message_dict["welcome"])
-
-rps_computer = Player(0)
-rps_player = Player(0)
-play_game()
-=======
             self.play_game()
->>>>>>> featuredev:core_game_rps.py
 
 rpsgame = Game_rps() # create instance of the game passing in arguments to set the player score, computer score and count of matches played
 rpsgame.play_game() # call a method on the instance of the game to start play
