0d8ddd5862b6a2c20cf1271afaaf61cd2944703a
Ellen Houghton <ellen.a.ash@gmail.com>
2023-02-26 17:12:30 +0000

    RPS game now working, fully encapsulated in a class and readme updated

--- README.md
+++ README.md
@@ -1,15 +1,13 @@
 # Command Line Games
 
-A selection of command line games in python to demonstrate programming knowledge built around a [Games class](./games/games.py).
+A selection of command line games in python to demonstrate programming knowledge.
 
 **Rock, Paper, Scissors**
 
-- A small command line game that has multiple functions linking across 2 files, using imported modules (sys, time) and features from a class; Games that I built.
-- The game also offers tha player the option to save the end score to a file. 
-- The game has an in-game message dictionary that is separate from the code where game messages can be edited without needing to trawl through the code. 
-- The code also demonstrates my programming knowledge of **kwargs, tuple unpacking and positionally based arguments in function parameters.
+- encapsulated with in a class
+- abstracting the scoring system and the count of matches played to be the instance of the class
+- scores are saved to a [txt file](./saved_score.txt)
     - Link to [core game code](core_game_rps.py)
-    - Link to [RPS Feature Details](README_rps.md)
 
 ## Instructions to play:
 **Rock, Paper, Scissors** run:
@@ -19,6 +17,5 @@ python core_game_rps.py
 
 Challenges Overcome:
 - forgetting to create .gitignore after creating command line repo, and listing my entire venv for commit!
-- editing the scoring system from a function on another page tracking the sacores as parameters through all the other functions called, into a Player class that tracks the scores independently was a challenge but also demonstrated the real benefits of using classes to simplify my code
+- initially I was tracking the scores in a function but when I revisited the game to encapsulate this in a class, the game code became much clearer to read and easier to edit
 - learning the benefits of having version control and a feature branch so my repo always has current, working code on the main branch while I develop on the features branch
-
--- core_game_rps.py
+++ core_game_rps.py
@@ -11,79 +11,94 @@ ingame_message_dict = {
     "get_ready": [" Ready    ", "Set   ", "Go\n"], 
     'welcome': ["Welcome to...\n", "Rock\t", "Paper\t", "Scissors\t\n\n"],
     "sorry": "Sorry, I did not understand your choice. \n",
-    'end_game_message': "Goodbye"
                        }
 
-# ABSTRACTION - the object here is the score that I need to track through the game for both player and computer and not the game title etc
-# ENCAPSULATION - the score tracker is encapsulated here so it is not dragged through functions and over complicating the game
+# ABSTRACTION - the object here is the score and match count that I need to track through the game for both player and computer and not the game title etc
+# ENCAPSULATION - the score tracker is encapsulated here so it is not dragged through functions and over complicating the game code
 
 class Game_rps:
 
-    def __init__(self, opening_score):
-        self._score = opening_score
+    def __init__(self, player_score=0, computer_score=0, match_count=0):
+        self._player_score = player_score
+        self._computer_score = computer_score
+        self._match_count = match_count
 
     # BEHAVIOUR
-    def increase_score(self):
-        self._score += 1
+    def increase_score(self, winner):
+        if winner == "player":
+            self._player_score += 1
+        else:
+            self._computer_score += 1
     
     # getter DATA
-    def get_score(self):
-        return self._score
+    def get_player_score(self):
+        return self._player_score
     
-    def end_game():
+    # getter DATA
+    def get_computer_score(self):
+        return self._computer_score
+    
+    def increase_match_count(self):
+        self._match_count += 1
+    
+    # getter DATA
+    def get_match_count(self):
+        return self._match_count
+    
+    def end_game(self):
         print("Goodbye\n", "="*50)
         sys.exit()
     
     def validate(self, data_to_check):
-        self.data_to_check = data_to_check
+        self.data_to_check = data_to_check.upper()
         user_dict = {"R": "Rock", "P": "Paper", "S": "Scissors"}
-        while data_to_check.upper() not in user_dict.keys():
-            if data_to_check == "Q":
-                Game_rps.save_game()
-            else:
-                print(ingame_message_dict["sorry"])
-                data_to_check = input("\n\t(R)ock\n\t(P)aper\n\t(S)cissors\n\t(Q)uit\n")
-        return user_dict[data_to_check]
+        if self.data_to_check in user_dict.keys():
+            return user_dict[self.data_to_check]
+        else:
+            while self.data_to_check not in user_dict.keys():
+                if self.data_to_check == "Q" or self.data_to_check == "q":
+                    self.save_game()
+                else:
+                    print(ingame_message_dict["sorry"])
+                    self.data_to_check = input("\n\t(R)ock\n\t(P)aper\n\t(S)cissors\n\t(Q)uit\n")
     
-    def congratulations():
+    def congratulations(self):
         winner_message = ingame_message_dict["winner_u"]
         print("*" * 6 + "*" * len(winner_message) + "*" * 6)
         print("*" * 6 + winner_message + "*" * 6)
         print("*" * 6 + "*" * len(winner_message) + "*" * 6)
         print()
 
-    def save_game():
-        save = input("Do you wish to save the scores? Y or N")
+    def save_game(self):
+        save = input("Do you wish to save the scores? Y or N ")
         if save in ["n", "N", 'q', "Q"]:
-            Game_rps.end_game(ingame_message_dict["end_game_message"])
+            self.end_game()
         else:
             now = datetime.now()
             date_time_str = now.strftime("%Y-%m-%d %H:%M")
             with open("saved_score.txt", 'a') as save_score:
-                save_score.write(f"Rock, Paper, Scissors  {rps_computer.get_score()}:{rps_player.get_score()}  {date_time_str}\n")
-            Game_rps.end_game(ingame_message_dict["end_game_message"])
+                save_score.write(f"Rock, Paper, Scissors  {self.get_computer_score()}:{self.get_player_score()}  {date_time_str}\n")
+            self.end_game()
 
 
-    def play_game():
+    def play_game(self):
 
-        # jazzy game opening
-        for item in ingame_message_dict['welcome']:
-            time.sleep(0.85)
-            print(item, end = '', flush = True)
-    
-        # progress bar to appear as if the computer is "thinking"
-        print("Lets choose now\nI am thinking")
-        for i in range(26):
-            time.sleep(0.15)
-            print('.', end = '', flush=True)
-
-        # create choices
+        # use the jazzy game opening and progress bar on the first match only
+        if self.get_match_count() == 0:
+            for item in ingame_message_dict['welcome']:
+                time.sleep(0.85)
+                print(item, end = '', flush = True)
+            print("Lets choose now\nI am thinking")
+            for _ in range(26):
+                time.sleep(0.15)
+                print('.', end = '', flush=True)
+        
+        #game logic
         computer = random.choice(["Rock", "Paper", "Scissors"])
         data_to_check = input("\n\t(R)ock\n\t(P)aper\n\t(S)cissors\n\t(Q)uit\n")
         user = self.validate(data_to_check)
         print(f"\tYou chose {user}\n\tand\t\n\tComputer chose {computer}\n")
     
-        #game logic
         winner = ""
         if user == computer:
             print(ingame_message_dict["draw"])
@@ -91,42 +106,38 @@ class Game_rps:
         elif user == "Rock":
             if computer == "Paper":
                 print(ingame_message_dict["paper_win"], ingame_message_dict["winner_c"])
-                winner = "computer"
+                self.increase_score("computer")
             else:
                 print(ingame_message_dict["rock_win"])
                 self.congratulations()
-                winner = "player"
+                self.increase_score("player")
         elif user == "Paper":
             if computer == "Scissors":
                 print(ingame_message_dict["scissors_win"], ingame_message_dict["winner_c"])
-                winner = "computer"
+                self.increase_score("computer")
             else:
                 print(ingame_message_dict["paper_win"])
-                congratulations()
-                winner = "player"
+                self.congratulations()
+                self.increase_score("player")
         else:  # if the human_player chooses Scissors
             if computer == "Rock":
                 print(ingame_message_dict["rock_win"], ingame_message_dict["winner_c"])
-                winner = "computer"
+                self.increase_score("computer")
             else:
                 print(ingame_message_dict["scissors_win"])
-                congratulations()
-                winner = "player"
-
-        #scoring
-        if winner == "computer":
-            rps_computer.increase_score(1)
-        else:
-            rps_player.increase_score(1)
-        print(f"Your score is {rps_player.get_score()} and the computer has {rps_computer.get_score()}.")
-
+                self.congratulations()
+                self.increase_score("player")
+    
+        # end of match, print score and count matches
+        self.increase_match_count()
+        print(f"Your score is {self.get_player_score()} and the computer has {self.get_computer_score()}.\nGames played {self.get_match_count()}")
+        
         # play again
         decision = input("Press enter to play again or Q to quit\n")  # Q is hardcoded so this text is not available to modify in the game dictionary
         if decision in ["Q", "q"]:
-            Game_rps.save_game()
+            self.save_game()
         else:
-            Game_rps.play_game()
+            self.play_game()
 
-rps_computer = Game_rps(0)
-rps_player = Game_rps(0)
-Game_rps.play_game()
+rpsgame = Game_rps() # create instance of the game passing in arguments to set the player score, computer score and count of matches played
+rpsgame.play_game() # call a method on the instance of the game to start play
