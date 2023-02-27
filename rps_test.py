a9f2f8cfd31e70d4b194b9a8024833d2dea90b30
Ellen Houghton <ellen.a.ash@gmail.com>
2023-02-27 15:06:12 +0000

    pytest works but not my test, yet!

--- rps_test.py
+++ rps_test.py
@@ -1,9 +1,37 @@
 import pytest
 import rps_core_game
 
+# DON'T FORGET TO TYPE PYTEST INSTEAD OF PYTHON
+
+# # Example from webpage
+# def func(x):
+#     return x + 1
+
+# def test_answer():
+#     assert func(3) == 5
+
+def game_logic(computer_choice, user_choice):
+    winner = ""
+    if user_choice == computer_choice:
+        # print(ingame_message_dict["draw"])
+        winner = "None"
+    # winning scenarios for computer
+    elif (user_choice == "Rock" and computer_choice == "Paper") or (user_choice == "Paper" and computer_choice == "Scissors") or (user_choice == "Scissors" and computer_choice == "Rock"):
+        winner = "computer"
+        # self.result_message(computer_choice)
+        # self.increase_score("computer")
+    else:
+        winner = "player"
+        # self.result_message(user_choice)
+        # self.increase_score("player")
+    # self.set_winner(winner)
+    return winner
+
+
+
 def check_game_logic():
     computer_choice = "Rock"
     user_choice = "Paper"
-    results = rps_core_game.rpsgame.game_logic(computer_choice, user_choice)
-    assert results == "None", "broken"
+    results = game_logic(computer_choice, user_choice)
+    assert results == "None"
 
