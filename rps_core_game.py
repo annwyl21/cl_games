3b4c7dac34647477912885ace0b3675670a43eab
Ellen Houghton <ellen.a.ash@gmail.com>
2023-02-27 17:47:14 +0000

    working code, play again function restored, now counts matches played

--- rps_core_game.py
+++ rps_core_game.py
@@ -10,7 +10,8 @@ ingame_message_dict = {
     "draw": "\t<<<<< DRAW >>>>>\t\n", 
     "get_ready": [" Ready    ", "Set   ", "Go\n"], 
     'welcome': ["Welcome to...\n", "Rock\t", "Paper\t", "Scissors\t\n\n"],
-    "sorry": "Sorry, I did not understand your choice. \n",
+    "offer_choice": "\n\t(R)ock\n\t(P)aper\n\t(S)cissors\n\t(Q)uit\n",
+    "sorry": "Sorry, I did not understand your choice.\n",
                        }
 
 # ABSTRACTION - the object here is the score and match count that I need to track through the game for both player and computer and not the game title etc
@@ -28,24 +29,24 @@ class Game_rps:
             self._player_score += 1
         else:
             self._computer_score += 1
+
+    def increase_match_count(self):
+        self._match_count += 1
+
+    def set_winner(self, winner):
+        self._winner = winner
     
-    # getter DATA
     def get_player_score(self):
         return self._player_score
     
-    # getter DATA
     def get_computer_score(self):
         return self._computer_score
     
-    def increase_match_count(self):
-        self._match_count += 1
-    
-    # getter DATA
     def get_match_count(self):
         return self._match_count
     
-    def set_winner(self, winner):
-        self._winner = winner
+    def get_draws(self):
+        return self._draws
 
     def get_winner(self):
         return self._winner
@@ -53,31 +54,36 @@ class Game_rps:
     def end_game(self):
         print("Goodbye\n", "="*50)
         sys.exit()
-    
-    def validate(self, data_to_check):
-        self.data_to_check = data_to_check.upper()
-        user_dict = {"R": "Rock", "P": "Paper", "S": "Scissors"}
-        if self.data_to_check in user_dict.keys():
-            return user_dict[self.data_to_check]
-        else:
-            while self.data_to_check not in user_dict.keys():
-                if self.data_to_check == "Q" or self.data_to_check == "q":
-                    self.save_game()
-                else:
-                    print(ingame_message_dict["sorry"])
-                    self.data_to_check = input("\n\t(R)ock\n\t(P)aper\n\t(S)cissors\n\t(Q)uit\n")
 
-    def game_logic(self, computer_choice, user_choice):
-        if user_choice == computer_choice:
+    def play_again(self):
+        decision = input("Press enter to play again or (Q)uit\n")  # Q is hardcoded so this text is not available to modify in the game dictionary
+        if decision == "Q" or decision == "q":
+            self.save_game()
+        else:
+            self.play_game()
+    
+    def make_choice(self):
+        player_input = input(ingame_message_dict["offer_choice"]).upper()
+        choice_dict = {"R": "Rock", "P": "Paper", "S": "Scissors"}
+        if player_input in choice_dict.keys():
+            return choice_dict[player_input]
+        elif player_input == "Q" or player_input == "q":
+            self.save_game()
+        else:
+            print(ingame_message_dict["sorry"])
+            self.play_again()
+            
+    def game_logic(self, computer_choice, player_choice):
+        if player_choice == computer_choice:
             print(ingame_message_dict["draw"])
         # winning scenarios for computer
-        elif (user_choice == "Rock" and computer_choice == "Paper") or (user_choice == "Paper" and computer_choice == "Scissors") or (user_choice == "Scissors" and computer_choice == "Rock"):
+        elif (player_choice == "Rock" and computer_choice == "Paper") or (player_choice == "Paper" and computer_choice == "Scissors") or (player_choice == "Scissors" and computer_choice == "Rock"):
             self.set_winner("computer")
             self.result_message(computer_choice)
             self.increase_score("computer")
         else:
             self.set_winner("player")
-            self.result_message(user_choice)
+            self.result_message(player_choice)
             self.increase_score("player")
     
     def result_message(self, choice):
@@ -99,7 +105,7 @@ class Game_rps:
             now = datetime.now()
             date_time_str = now.strftime("%Y-%m-%d %H:%M")
             with open("saved_score.txt", 'a') as save_score:
-                save_score.write(f"Rock, Paper, Scissors  {self.get_computer_score()}:{self.get_player_score()}  {date_time_str}\n")
+                save_score.write(f"Rock, Paper, Scissors  {self.get_computer_score()}:{self.get_player_score()}  {date_time_str} Matches played: {self.get_match_count()}")
             self.end_game()
 
     def play_game(self):
@@ -116,23 +122,18 @@ class Game_rps:
         
         # making rock, paper, scissors choices
         computer_choice = random.choice(["Rock", "Paper", "Scissors"])
-        data_to_check = input("\n\t(R)ock\n\t(P)aper\n\t(S)cissors\n\t(Q)uit\n")
-        user_choice = self.validate(data_to_check)
-        print(f"\tYou chose {user_choice}\n\tand\t\n\tComputer chose {computer_choice}\n")
+        player_choice = self.make_choice()
+        print(f"\tYou chose {player_choice}\n\tand\t\n\tComputer chose {computer_choice}\n")
 
         # calling game logic
-        self.game_logic(computer_choice, user_choice)
+        self.game_logic(computer_choice, player_choice)
     
         # end of match, print score and count matches
         self.increase_match_count()
-        print(f"Your score is {self.get_player_score()} and the computer has {self.get_computer_score()}.\nGames played {self.get_match_count()}")
+        print(f"Your score is {self.get_player_score()} and the computer has {self.get_computer_score()}.\nGames played {self.get_match_count()}\n")
         
-        # play again
-        decision = input("Press enter to play again or Q to quit\n")  # Q is hardcoded so this text is not available to modify in the game dictionary
-        if decision in ["Q", "q"]:
-            self.save_game()
-        else:
-            self.play_game()
+        # offer to play again
+        self.play_again()
 
 rpsgame = Game_rps() # create instance of the game passing in arguments to set the player score, computer score and count of matches played
 
