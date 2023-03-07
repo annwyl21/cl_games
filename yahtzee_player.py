c22d28f76604452bcd1afb017a27e5aad0fcab1b
Ellen Houghton <ellen.a.ash@gmail.com>
2023-03-07 09:52:41 +0000

    working on computer taking a turn first

--- yahtzee_player.py
+++ yahtzee_player.py
@@ -26,31 +26,48 @@ class Yahtzee_player:
                              }
          
     def get_dice_roll(self):
-        return self._dice_roll
-    
-    def player_turn(self):
-        if self._player_type == "computer":
-            self._dice_roll = [random.randrange(1, 7) for i in range(0, 5)]
-        self.update_computer_turn()
-    
-    def update_computer_turn():
-        for score in self._
-    
-    def calculate_score(self):
-        # determine player_type & random dice roll so never call this more than once!
-        self.player_turn()
-        # create an instance to send to the calculator
-        self._score_dictionary = Yahtzee_calc(self._dice_roll).calculator()
+        return self._dice_roll 
+
+    def calculate_computer_score(self):
+        # create a random dice roll for the computer
+        self._dice_roll = [random.randrange(1, 7) for i in range(0, 5)]
+        
+        # create an instance to send to the calculator mehod in the calc class
+        self._score_dictionary = Yahtzee_calc(self.get_dice_roll()).calculator()
         #print(self._player_name.get_score_dict())
 
-        # update computer score
+        # update computer score with either highest score or a score higher than zero
         highest_score = 0
-        for key, score in self._score_dictionary.items():
+        for roll_type, score in self._score_dictionary.items():
             if score > highest_score:
                 highest_score = score
-                if self._game_dictionary[key] == 0:
-                    self._game_dictionary[key] = score
+                if self._game_dictionary[roll_type] == 0:
+                    self._game_dictionary[roll_type] = score
+            else:
+                for myref, number in self._score_dictionary.items():
+                    if number > 0:
+                        for stored_results in self._game_dictionary.values():
+                            if stored_results == 0:
+                                self._game_dictionary[myref] = number
+        return self._score_dictionary
+        
+    def calculate_player_score(self):
+        pass
+    #     # calculate the score using the players dice roll
+    #     self._score_dictionary = Yahtzee_calc(self._dice_roll).calculator()
 
+    #     # display the score choices
+    #     print("Here are the possible scores available from your dice roll...")
+    #     print("Number to pick result - Results to choose from - Current Game Scores Held")
+    #     for key, value in enumerate()
+
+
+
+    def player_turn(self):
+        if self._player_type == "computer":
+            self._score_dictionary = self.calculate_computer_score()
+        else:
+            self._score_dictionary = self.calculate_player_score()
         return self._score_dictionary
 
         
@@ -58,5 +75,7 @@ if __name__ == "__main__":
     # create 1 player instance of a turn of play for a computer
     computer1 = Yahtzee_player("computer", [])
     
-    print(computer1.calculate_score())
+    print(computer1.player_turn())
     print(computer1.get_dice_roll())
+
+    # computer turn not working
